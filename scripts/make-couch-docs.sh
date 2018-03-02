f9d58aba4d0c495de8cda8ba46a077cc33ec398c
epatters <ejpatters@gmail.com>
2018-03-02 14:08:06 -0500

    Remove _id field from documents.
    The _id field is CouchDB-specific, hence has no business being here.

--- scripts/make-couch-docs.sh
+++ scripts/make-couch-docs.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-find concept annotation -name "*.json" -exec cat {} + |
+{ 
+    find concept -name "*.json" -exec cat {} + |
+        jq '{_id: ("concept/"+.ontology+"/"+.id)} + .';
+    
+    find annotation -name "*.json" -exec cat {} + |
+        jq '{_id: ("annotation/"+.language+"/"+.package+"/"+.id)} + .';
+} |
     jq -s '.'
