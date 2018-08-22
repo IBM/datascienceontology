774c491fe65208d5693df07864d95ca55d17b276
epatters <ejpatters@gmail.com>
2018-08-21 21:20:18 -0700

    Create NPM package to manage build dependencies.
    Dependencies for the "build" and "validate" scripts can now be
    installed with "one click".

--- tools/build.sh
+++ tools/build.sh
@@ -3,6 +3,7 @@ set -e
 
 DIR=$(dirname $0)
 cd "$DIR/.."
+PATH="node_modules/.bin:$PATH"
 
 # Create build directory structure.
 rm -rf build
@@ -10,5 +11,5 @@ find concept annotation -type d | xargs -I{} mkdir -p "build/{}"
 
 # Convert YAML documents to JSON.
 find concept annotation -name '*.yml' | while read yml; do
-    yq '.' "$yml" > "build/${yml%.yml}.json"
+    js-yaml "$yml" > "build/${yml%.yml}.json"
 done
--- tools/validate.sh
+++ tools/validate.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 DIR=$(dirname $0)
-cd "$DIR/../build"
+cd "$DIR/.."
+PATH="node_modules/.bin:$PATH"
 
-ajv --errors=json -s "../tools/schemas/concept.json" -d "concept/*.json"
+ajv --errors=json -s "tools/schemas/concept.json" -d "build/concept/*.json"
 (( STATUS = STATUS || $? ))
 
-ajv --errors=json -s "../tools/schemas/annotation.json" -d "annotation/**/*.json"
+ajv --errors=json -s "tools/schemas/annotation.json" -d "build/annotation/**/*.json"
 (( STATUS = STATUS || $? ))
 
 exit $STATUS
