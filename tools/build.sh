d56f4cf3b814b74df78b34f4e37aec16999181df
Evan Patterson <ejpatters@gmail.com>
2019-03-01 16:02:23 -0800

    Update build scripts to support references.
    Towards #13. Output conforms to the CSL JSON schema:
    
    https://github.com/citation-style-language/schema/blob/master/csl-data.json

--- tools/build.sh
+++ tools/build.sh
@@ -7,9 +7,14 @@ PATH="node_modules/.bin:$PATH"
 
 # Create build directory structure.
 rm -rf build
-find concept annotation -type d | xargs -I{} mkdir -p "build/{}"
+find concept annotation references -type d | xargs -I{} mkdir -p "build/{}"
 
-# Convert YAML documents to JSON.
+# Convert YAML concept and annotation documents to JSON.
 find concept annotation -name '*.yml' | while read yml; do
     js-yaml "$yml" > "build/${yml%.yml}.json"
+done
+
+# Convert BibTeX references to CSL JSON.
+find references -name '*.bib' | while read bib; do
+    pandoc-citeproc --bib2json "$bib" > "build/${bib%.bib}.json"
 done
--- tools/make-couch-docs.sh
+++ tools/make-couch-docs.sh
@@ -10,5 +10,8 @@ cd "$DIR/../build"
     
     find annotation -name "*.json" -exec cat {} + |
         jq '{_id: ("annotation/"+.language+"/"+.package+"/"+.id)} + .';
+    
+    find references -name "*.json" -exec cat {} + |
+        jq '.[]' | jq '{_id: ("reference/"+.id), schema: "csl-data-item"} + .';
 } |
     jq -s '.'
