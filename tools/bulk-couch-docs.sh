d658288a7415beaba2f8e07527f79131ea2af6d9
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2019-11-30 23:02:27 +0100

    Write CouchDB docs to a temporary file before bulk updating.
    Piping into ccurl does not appear to work on my Linux machine (Bash 5).

--- tools/bulk-couch-docs.sh
+++ tools/bulk-couch-docs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 DIR=$(dirname $0)
+PATH="node_modules/.bin:$PATH"
+TMP_DOCS_FILE="$DIR/../build/all-docs.json"
 
-"$DIR/make-couch-docs.sh" |
-    jq '{docs: .}' |
-    ccurl -X POST -d @- /data-science-ontology/_bulk_docs |
-    jq '.'
+"$DIR/make-couch-docs.sh" | jq '{docs: .}' > $TMP_DOCS_FILE
+ccurl -X POST -d @$TMP_DOCS_FILE /data-science-ontology/_bulk_docs | jq '.'
